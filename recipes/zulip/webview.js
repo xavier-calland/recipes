5063368b3cadab15ed4304efeae0649cbfee036c
Xavier Calland <xca@atolcd.com>
2022-04-26 10:06:20 +0200

    [Zulip] Fix message count
    and update icon

--- recipes/zulip/webview.js
+++ recipes/zulip/webview.js
@@ -1,12 +1,27 @@
 module.exports = Ferdi => {
   const getMessages = () => {
-    const allMessages = Math.round(
-      document.querySelectorAll(
-        '#global_filters .top_left_all_messages .count .value',
-      )[0].textContent,
-    );
-    Ferdi.setBadge(allMessages);
+    // All unread messages
+    const unreadAll = getUnreadCount('top_left_all_messages');
+
+    // Private messages
+    const unreadPrivate = getUnreadCount('top_left_private_messages');
+
+    // @ Mentions messages
+    const unreadMentions = getUnreadCount('top_left_mentions');
+
+    const directMessages = unreadPrivate + unreadMentions;
+    const indirectMessages = unreadAll - directMessages;
+
+    Ferdi.setBadge(directMessages, indirectMessages);
   };
 
   Ferdi.loop(getMessages);
+
+  function getUnreadCount(eltClassName) {
+    const elt = document.querySelectorAll(`#global_filters .${eltClassName} .unread_count`)[0];
+    if (elt == null) {
+      return 0;
+    }
+    return Ferdi.safeParseInt(elt.textContent);
+  }
 };
